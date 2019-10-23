da02e6c375cbd5fe81b154b5dd3d720683463ada
Alexell <alexell@alexell.ru>
2019-10-23 12:27:08 +0300

    Добавлена английская локализация.

---
+++ lua/metrostroi_data/languages/en_mgb.lua
@@ -0,0 +1,44 @@
+return [[
+[en]
+
+AuthorTextMGB = Agent Smith (STEAM_0:1:15049625)
+
+MGB.GUI.Tabs.Online.Title = Players online
+MGB.GUI.Tabs.Online.SendReport = Send report
+MGB.GUI.Tabs.Bad.Title = Players with reports
+MGB.GUI.Tabs.Wait.Title = Vote for ban
+MGB.GUI.Tabs.Wait.Yes = Yes
+MGB.GUI.Tabs.Wait.No = No
+MGB.GUI.Tabs.Wait.Vote = Voting
+MGB.GUI.Tabs.Ban.Title = Global bans
+MGB.GUI.Tabs.Ban.Date = Ban date
+MGB.GUI.AddReport.Title = Send a report on player
+MGB.GUI.AddReport.Rules = "Основные заметки и правила перед отправкой репорта.\nБла бла бла\nбла бла бла"
+MGB.GUI.AddReport.Headline = Report info
+MGB.GUI.AddReport.Textleft = left
+MGB.GUI.AddReport.Characters = symbol(s)
+MGB.GUI.AddReport.TextEmpty = Specify the report info
+MGB.GUI.AddReport.Send = Send
+MGB.GUI.Reports.Title = Player's reports
+MGB.GUI.Reports.Server = Server
+MGB.GUI.Reports.Date = Date
+MGB.Labels.Player = Player
+MGB.Labels.Nick = Nickname
+MGB.Labels.Group = Group
+MGB.Labels.Report = Report
+MGB.Labels.Reports = Reports
+MGB.Labels.ReportList = Report list
+MGB.Labels.Copy = Copy
+MGB.Messages.EventReport = recieved a report in server
+MGB.Messages.EventBan = recieved a global ban
+MGB.Messages.NoticeBad = You have new player reports
+MGB.Messages.NoticeWait = You have new ban votes in progress
+MGB.Messages.WebRequestFailed = API request error! Try again later.
+MGB.Messages.Serverblocked = Access denied for %s! Please contact the addon developer.
+MGB.Messages.NoReports = No reports found!
+MGB.Messages.ReportAdded = Report sent successfully!
+MGB.Messages.ReportRepeat = Server %s already sent report on this player!
+MGB.Messages.VoteAdded = Your vote has been sent! 
+MGB.Messages.VoteRepeat = Server %s already voted for this player!
+
+]]
